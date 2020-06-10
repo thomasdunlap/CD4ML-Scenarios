2d774ddb2540d87309dbae1ffaec53494a8db0ac
thomasdunlap <tom.a.dunlap@gmail.com>
2020-06-10 16:45:18 -0700

    Change to 100 trees.

--- cd4ml/ml_model_params.py
+++ cd4ml/ml_model_params.py
@@ -1,13 +1,13 @@
 model_parameters = {
     "random_forest": {
-        "n_estimators": 10,
+        "n_estimators": 100,
         "max_features": 0.5
     },
     "adaboost": {
-        "n_estimators": 100
+        "n_estimators": 200
     },
     "gradient_boosting": {
-        "n_estimators": 200,
+        "n_estimators": 300,
         "max_depth": 4
     },
     "decision_tree": {
